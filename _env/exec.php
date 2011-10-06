02b96787c94fd53c190a1365aa1d11029c89e145
christoph.ackermann <info@acki.be>
2011-10-06 09:47:54 +0200

    Added logging functionality

--- _env/exec.php
+++ _env/exec.php
@@ -4,12 +4,20 @@
     * System-wide include file
     * Execute important things, include configs and functions, do some magic stuff
     */
+    
+   /**
+    * We need the configuration from the beginning
+    */
+    include('config.default.php');
 
    /**
-    * Sets the value to report all PHP error
+    * Sets the value to report all PHP error, set log file, set locales
     */
     error_reporting(E_ALL, E_NOTICE);
-   
+    ini_set('error_log','../' . $cfg['log']['errordir'] . '/' . $cfg['log']['errorlog']);
+    ini_set('log_errors', $cfg['log']['error']);
+    setlocale(LC_TIME, $cfg['locales']['LC_TIME']);
+    
    /**
     * Create an output cache
     */ 
@@ -26,6 +34,5 @@
    /**
     * Includes
     */
-    include('config.default.php');
     
 ?>
