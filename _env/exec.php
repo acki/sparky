ef510395ac8b2541f737a7195a56526ed9984850
christoph.ackermann <info@acki.be>
2011-10-06 08:58:19 +0200

    Reorganized exec.php, added some nice comments, added config include

--- _env/exec.php
+++ _env/exec.php
@@ -1,15 +1,31 @@
 <?php
 
+   /**
+    * System-wide include file
+    * Execute important things, include configs and functions, do some magic stuff
+    */
+
+   /**
+    * Sets the value to report all PHP error
+    */
     error_reporting(E_ALL, E_NOTICE);
-    
+   
+   /**
+    * Create an output cache
+    */ 
     ob_start();
     
-    include('functions/error.php');
-        
+   /**
+    * Error handling for direct file access, before do more stuff
+    */
+    include('functions/error.php');    
     if(stristr($_SERVER["REQUEST_URI"], 'exec.php') === 'exec.php') {
-
         panic("You shouldn't access this file directly", "../");
-    
     }
+    
+   /**
+    * Includes
+    */
+    include('config.default.php');
     
 ?>
