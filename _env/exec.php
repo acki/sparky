e76367381a31740a6529f1f98bdda280330b252f
christoph.ackermann <info@acki.be>
2011-10-06 15:58:32 +0200

    Comment styling

--- _env/exec.php
+++ _env/exec.php
@@ -5,34 +5,25 @@
     * Execute important things, include configs and functions, do some magic stuff
     */
     
-   /**
-    * We need the configuration from the beginning
-    */
+    // We need the configuration from the beginning
     include('config.default.php');
 
-   /**
-    * Sets the value to report all PHP error, set log file, set locales
-    */
+    // Sets the value to report all PHP error, set log file, set locales
     error_reporting(E_ALL, E_NOTICE);
     ini_set('error_log','../' . $cfg['log']['errordir'] . '/' . $cfg['log']['errorlog']);
     ini_set('log_errors', $cfg['log']['error']);
     setlocale(LC_TIME, $cfg['locales']['LC_TIME']);
     
-   /**
-    * Create an output cache
-    */ 
+    // Create an output cache
     ob_start();
     
-   /**
-    * Error handling for direct file access, before do more stuff
-    */
+    // Error handling for direct file access, before do more stuff
     include('functions/error.php');    
     if(stristr($_SERVER["REQUEST_URI"], 'exec.php') === 'exec.php') {
         panic("You shouldn't access this file directly", "../");
     }
     
-   /**
-    * Includes
-    */
-    
+    // Includes
+    include('functions/functions.php');
+        
 ?>
