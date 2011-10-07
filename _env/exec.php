456e1ebaeadf2ee3eac21154d7cb74e1397d62a3
christoph.ackermann <info@acki.be>
2011-10-07 15:37:55 +0200

    Added debug handler, added some other includes

--- _env/exec.php
+++ _env/exec.php
@@ -7,31 +7,39 @@
     
     // We need the configuration from the beginning
     include('config.default.php');
+    
+    // Check if constants are defined
+    if(!defined('dire')) {
+        define('dire', '../');
+    }
 
-    // Sets the value to report all PHP error, set log file, set locales
+    // Set error reporting
     error_reporting(E_ALL);
-    ini_set('error_log','../' . $cfg['log']['errordir'] . '/' . $cfg['log']['errorlog']);
-    ini_set('log_errors', $cfg['log']['error']);
-    setlocale(LC_TIME, $cfg['locales']['LC_TIME']);
+
+    // Check if debug mode
+    if($cfg['page']['debug']) {
+        // Unset cache
+        $cache = false;
+    } else {
+        // Set no error display
+        ini_set('display_errors', 0);
+        
+        // Log errors to file
+        ini_set('error_log', dire . $cfg['log']['errordir'] . '/' . $cfg['log']['errorlog']);
+        ini_set('log_errors', $cfg['log']['error']);
+        
+        // Set template cache path
+        $cache = dire . $cfg['page']['cache'];
+    }
     
     // Create an output cache
     ob_start();
     
-    // Error handling for direct file access, before do more stuff
-    include('functions/error.php');    
-    if(stristr($_SERVER["REQUEST_URI"], 'exec.php') === 'exec.php') {
-        panic("You shouldn't access this file directly", "../");
-    }
-    
     // Create style path
     $tmp_style_path = dire . $cfg['style']['path'].'/' . $cfg['style']['id'].'/';
 
-    // Includes
-    include('functions/functions.php');
-    include('functions/render.php');
-    include('classes/packages.php');
-    
     // Autoload packages
+    include('classes/packages.php');
     $packages = new Package;
     $packages->loadPackages($cfg['packages']['required']);
     
@@ -39,10 +47,21 @@
     Twig_Autoloader::register();
     $loader = new Twig_Loader_Filesystem($tmp_style_path . '/templates');
     $twig = new Twig_Environment($loader, array(
-        'cache' => $cfg['page']['cache'],
+        'cache' => $cache,
     ));
+
+    // Includes
+    include('functions/functions.php');
+    include('functions/render.php');
     
-    // Set encoding
+    // Error handling for direct file access, before do more stuff
+    include('functions/error.php');    
+    if(stristr($_SERVER["REQUEST_URI"], 'exec.php') === 'exec.php') {
+        panic("You shouldn't access this file directly", "../");
+    }
+
+    // Set encoding and locales
     header('Content-Type: text/html; charset=UTF8');
+    setlocale(LC_TIME, $cfg['locales']['LC_TIME']);
     
 ?>
