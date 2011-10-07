9218808c2129d19b56f076981fa9042d16562029
christoph.ackermann <info@acki.be>
2011-10-07 15:36:42 +0200

    Added panic handler

--- _env/functions/error.php
+++ _env/functions/error.php
@@ -8,7 +8,7 @@
      */
 
     function panic($panic = 'Unknown error!', $dire = dire) {
-    
+        
         // Clean the web output
         ob_end_clean();
         
@@ -18,52 +18,20 @@
         // Read the needed backtrace informations, get script name
         $debug = debug_backtrace();
         if(isset($debug[0])) {
-            $file       = $debug[0]['file'];
-            $line       = $debug[0]['line'];
+            $data['file']       = $debug[0]['file'];
+            $data['line']       = $debug[0]['line'];
         }
         
-        // Get the simple script name
-        $file = basename($file);
-    
+        // Get the simple script name and set data
+        $data['file'] = basename($data['file']);
+        $data['panic'] = $panic;
+        
         // Print the panic message well formatted
-        print '
-<html>
-    <head>
-        <title>sparky runned into a wall</title>
-        <style>
-            body {
-                font-family: Helvetica, Arial, Verdana, sans-serif;
-                font-size: 14px;
-                text-align: center;
-            }
-            pre {
-                margin-bottom: 20px;
-                color: darkred;
-            }
-            a {
-                color: darkred;
-            }
-        </style> 
-    </head>
-    <body>
-
-        <h1><img src="' . $dire . '_style/default/icons/deadly.png" alt="deadly" /> Eh. I am <a href="http://github.com/chrigu99/sparky" target="_blank">sparky</a>.<br />I\'ve run into a wall.</h1>
-        
-        <h2>The reason i think:</h2>
-        <pre>' . $panic . '</pre>
-        
-        <h4>File:</h4>
-        <pre>' . $file . '</pre>
-        <h4>Line:</h4>
-        <pre>' . $line . '</pre>
-        
-    </body>
-</html>
-        ';
+        render('panic.html', 'sparky runned into a wall', $data);
 
         // Don't do more, better you die.
         die;
         
     }
-
+    
 ?>
