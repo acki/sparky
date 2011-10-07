d16f1413b6d6f29e861f337c2401318515cc13b0
christoph.ackermann <info@acki.be>
2011-10-07 15:37:19 +0200

    Created a global rendering function

--- _env/functions/render.php
+++ _env/functions/render.php
@@ -1,11 +1,25 @@
 <?php
 
-    function render($template) {
+   /**
+    * Rendering function for Twig.
+    * Renders the template and submit some data
+    * @param    $template    Name of template
+    * @param    $title       Site title
+    * @param    $data        Array with data for the template rendering
+    */
+
+    function render($template, $title = 'Welcome', $data = array()) {
     
+        // Globalize some data
         global $twig, $tmp_style_path;
+        
+        // Append arrays
+        $defaultdata = array('title'=>$title, 'tmp_style_path'=>$tmp_style_path);
+        $data = $defaultdata + $data;
     
+        // Rendering template
         $template = $twig->loadTemplate($template);
-        print $template->render(array('title'=>'hallo', 'tmp_style_path'=>$tmp_style_path,));
+        print $template->render($data);
         
     }
     
